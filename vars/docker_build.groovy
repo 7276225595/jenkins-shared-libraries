386fe8ea09f138dc115755b598680f141b091800
7276225595 <102014390+7276225595@users.noreply.github.com>
2026-06-28 18:38:23 +0530

    Refactor docker_build to use parameters and tag images
    Refactor docker_build function to accept parameters directly and add tagging for Docker images.

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,12 +1,13 @@
-def call(Map config = [:]) {
-    def imageName = config.imageName ?: error("Image name is required")
-    def imageTag = config.imageTag ?: 'latest'
-    def dockerfile = config.dockerfile ?: 'Dockerfile'
-    def context = config.context ?: '.'
-    
-    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
-    
+def call(String imageName, String tag, String dockerHubUser) {
+
+    echo "Building Docker Image"
+
     sh """
-        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+        whoami
+        docker build -t ${imageName}:${tag} .
+    """
+
+    sh """
+        docker image tag ${imageName}:${tag} ${dockerHubUser}/${imageName}:${tag}
     """
 }
